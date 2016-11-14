7c5faa0b3d6efe7a50f2b6386454895579896692
ashnur <ashnur@gmail.com>
2016-11-14 16:15:22 +0100

    progress works

--- index.js
+++ index.js
@@ -12,16 +12,17 @@ const window = require("global/window")
 const width = +window.innerWidth
 const height =  +window.innerHeight
 
-const G = {nodes:[{}, {}, {}, {}]
+const G = {nodes:[{x:100}, {}, {}, {}]
   , links:[{source:0, target: 1, index:0}
           ,{source:1, target: 2, index:1}
           ,{source:2, target: 3, index:2}
           ]}
 const k = Math.sqrt(G.nodes.length / (width * height))
 const simulation = d3.forceSimulation()
-      .force("link", d3.forceLink().distance(20))
+      .force("link", d3.forceLink().distance(50))
       .force("charge", d3.forceManyBody().strength(-0.1 / k))
       .force("center", d3.forceCenter(width / 2, height / 3))
+//      .stop()
 
 simulation.nodes(G.nodes).on("tick", ticked)
 simulation.force("link").links(G.links)
@@ -49,11 +50,16 @@ var node = svg.append("g")
 
 node.append("title")
  .text(function(d, id) { return id })
-//
+
+
+const randEdge = require('random-number').generator({min: 0, max: G.links.length-1, integer: true})
+const initEdge = randEdge()
+
+
 const msg = svg.append("g")
 .attr("class", "msg")
 .selectAll("line")
-.data([G.links[2]])
+.data([G.links[initEdge]])
 .enter().append("line")
 .style("stroke", function(d) { return color(d.group)})
 
@@ -71,25 +77,47 @@ function ticked() {
     .attr("cx", function(d) { return d.x })
     .attr("cy", function(d) { return d.y })
 
+  msg
+    .attr("x1", function(d) { return d.source.x })
+    .attr("y1", function(d) { return d.source.y })
+    .attr("x2", function(d) { return d.target.x })
+    .attr("y2", function(d) { return d.target.y })
+
 }
 
+const byIndex = r.propEq('index')
 function update(){
   msg
     .transition()
     .duration(2000)
     .ease(d3.easeLinear)
-    .attrTween("x1", function(d, i, a) {
-      return function(){ return d.source.x }
+    .attrTween("x1", function(d) {
+      return function(){
+        return link.data().find(byIndex(d.index)).source.x
+      }
     })
-    .attrTween("y1", function(d, i, a) {
-      return function(){ return d.source.y }
+    .attrTween("y1", function(d) {
+      return function(){
+        return link.data().find(byIndex(d.index)).source.y
+      }
     })
     .attrTween("x2", function(d) {
-      return d3.interpolateNumber(d.source.x, d.target.x)
+      return function(t){
+        const p = link.data().find(byIndex(d.index))
+        //l(d3.interpolate(p.source.x, p.target.x)(t))
+        return d3.interpolate(p.source.x, p.target.x)(t)
+      }
     })
     .attrTween("y2", function(d) {
-      l(d.source.y, d.target.y)
-      return d3.interpolateNumber(d.source.y, d.target.y)
+      return function(t){
+        const p = link.data().find(byIndex(d.index))
+        //l(t, p.source.y, p.target.y, p.target.y - p.source.y)
+        return d3.interpolate(p.source.y, p.target.y)(t)
+      }
+    })
+    .on("end", function(d){
+      const line = this
+      
     })
   
 }
