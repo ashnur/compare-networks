ac5d7f7dcec9713d027d294006e40575102eaf88
ashnur <ashnur@gmail.com>
2016-11-18 09:28:24 +0100

    canvas example

--- index.js
+++ index.js
@@ -1,200 +1,90 @@
 const r = require("ramda")
 const d3 = require("d3")
 const rg = require('randomgraph')
-const l = console.log.bind(console)
-
-
-const svg = d3.select("#canvas")
-svg.attr("preserveAspectRatio", "xMidYMid meet")
-svg.attr({ "width": "100%", "height": "100%" })
-
+const document = require("global/document")
 const window = require("global/window")
+
+const canvasElement = document.createElement('canvas')
+document.body.appendChild(canvasElement)
 const width = +window.innerWidth
 const height =  +window.innerHeight
 
-// const G = {nodes:[{x:100}, {}, {}, {}]
-//   , links:[{source:0, target: 1, index:0}
-//           ,{source:1, target: 2, index:1}
-//           ,{source:2, target: 3, index:2}
-//           ]}
-//const G = rg.BalancedTree(3, 5)
-//const G = rg.WattsStrogatz.alpha(300, 4, 0.03)
-//const G = rg.ErdosRenyi.np(200, 0.)
-//const G = rg.BarabasiAlbert(300, 2, 1)
-const G = rg.BarabasiAlbert(300, 2, 2)
-const k = Math.sqrt(G.nodes.length / (width * height))
+const canvas = d3.select("canvas")
+    .attr("width", width)
+    .attr("height", height)
+
+const context = canvas.node().getContext("2d")
+
 const simulation = d3.forceSimulation()
-      .force("link", d3.forceLink().distance(50))
-      .force("charge", d3.forceManyBody().strength(-0.5 / k))
-      .force("center", d3.forceCenter(width / 2, height / 2))
-//      .stop()
+    .force("link", d3.forceLink().id(function(d) { return d.id; }))
+    .force("charge", d3.forceManyBody().strength(-10).distanceMax(300))
+    .force("center", d3.forceCenter(width / 2, height / 2))
+    .force("vertical", d3.forceY().strength(0.018))
+    .force("horizontal", d3.forceX().strength(0.006))
 
-simulation.nodes(G.nodes).on("tick", ticked)
-simulation.force("link").links(G.edges)
 
-var link = svg.append("g")
-  .attr("class", "links")
-  .selectAll("line")
-  .data(G.edges)
-  .enter().append("line")
-  .attr("stroke-width", function(d) { return 1 })
+const graph = rg.BarabasiAlbert(60, 2, 2)
 
-var color = d3.scaleOrdinal(d3.schemeCategory20)
+const nodes = graph.nodes.map((value, id)=>{return {id,value}})
+console.log(nodes, graph.nodes)
 
-var node = svg.append("g")
-.attr("class", "nodes")
-.selectAll("circle")
-.data(G.nodes)
-.enter().append("circle")
-.attr("r", 5)
-.attr("fill", function(d) { return 1 })
-.call(d3.drag()
-       .on("start", dragstarted)
-       .on("drag", dragged)
-       .on("end", dragended))
+simulation
+    .nodes(nodes)
+    .on("tick", ticked)
 
-node.append("title")
- .text(function(d, id) { return id })
+simulation.force("link")
+    .links(graph.edges)
 
-const rn = require('random-number')
-const randEdge = rn.generator({min: 0, max: G.edges.length-1, integer: true})
-const randNode = rn.generator({min: 0, max: G.nodes.length-1, integer: true})
-const initNode = randNode()
-const touches = r.curry((node, link) => {
-  const z = link.source.index == node || link.target.index == node
-  console.log(node, link, z, link.source.index, link.target.index)
-  return z
-})
-
-const messages = [initNode]
-
-function addDirections(link){
-  const to = messages.indexOf(link.source.index) > -1 ? link.target.index : link.source.index
-  const from = messages.indexOf(link.source.index) > -1 ? link.source.index : link.target.index
-  return r.merge({to, from}, link)
-}
-
-const currentEdges = G.edges.filter(touches(initNode)).map(addDirections)
-const drawnEdges = []
-console.log(currentEdges)
-
-const msg = svg.append("g").attr("class", "msg")
-
-function update(currentEdges){
-  const t = d3.transition().duration(500)
-  const t0 = d3.transition().duration(0)
-  const line = msg.selectAll("line").data(currentEdges)
-  
-  line.enter().append("line")
-    .style("stroke", function(d) { return color(d.group)})
-    .transition(t)
-    .ease(d3.easeLinear)
-    .attr("stroke-width", function(d){ return 3 })
-    .on("start", function repeat() {
-      const message = this
-      d3.active(message)
-        .attrTween("x1", function(d) {
-          return function(t){
-            const p = link.data().find(byIndex(d.index))
-            return p.source.index == d.from ? p.source.x : p.target.x
-          }
-        })
-        .attrTween("y1", function(d) {
-          return function(t){
-            const p = link.data().find(byIndex(d.index))
-            return p.source.index == d.from ? p.source.y : p.target.y
-          }
-        })
-        .attrTween("x2", function(d) {
-          return function(t){
-            const p = link.data().find(byIndex(d.index))
-            const source = p.source.index == d.from ? p.source.x : p.target.x
-            const target = p.source.index == d.from ? p.target.x : p.source.x
-            return d3.interpolate(source, target)(t)
-          }
-        })
-        .attrTween("y2", function(d) {
-          return function(t){
-            const p = link.data().find(byIndex(d.index))
-            const source = p.source.index == d.from ? p.source.y : p.target.y
-            const target = p.source.index == d.from ? p.target.y : p.source.y
-            return d3.interpolate(source, target)(t)
-          }
-        })
-    })
-    .transition(t0)
-    .on("end", function(d){
-      const line = this
-      currentEdges.forEach((ie) => drawnEdges.push(ie))
-      const lastTargets = currentEdges.map(r.prop('to'))
-      // add nodes to messages 
-      currentEdges.forEach((edge) => {
-        if ( messages.indexOf(edge.from) == -1 ) {
-          messages.push(edge.from)
-        }
-        if ( messages.indexOf(edge.to) == -1 ) {
-          messages.push(edge.to)
-        }
-      })
-      // select all edges from messages that lead to nodes that are not in messages
-      // console.log(d.from, d.to, d)
-      // console.log(currentEdges)
-      // console.log( messages)
-      const extendingEdges = G.edges.filter((link) => {
-        const x = (messages.indexOf(link.source.index) > -1 ) ^ (messages.indexOf(link.target.index) > -1 )
-        // console.log(messages, link.source.index, link.target.index, x)
-        return x
-      })
-      // console.log(extendingEdges)
-      const nextEdges = extendingEdges.map(addDirections)
-      // console.log(nextEdges)
-      update(currentEdges.concat(nextEdges))
-    })
-}
-
-update(currentEdges)
+canvas.call(d3.drag()
+        .container(canvas.node())
+        .subject(dragsubject)
+        .on("start", dragstarted)
+        .on("drag", dragged)
+        .on("end", dragended))
 
 function ticked() {
-  link
-    .attr("x1", function(d) { return d.source.x })
-    .attr("y1", function(d) { return d.source.y })
-    .attr("x2", function(d) { return d.target.x })
-    .attr("y2", function(d) { return d.target.y })
+  context.clearRect(0, 0, width, height)
 
-  node
-    .attr("cx", function(d) { return d.x })
-    .attr("cy", function(d) { return d.y })
+  context.beginPath()
+  graph.edges.forEach(drawLink)
+  context.globalAlpha = 0.12
+  context.strokeStyle = "#999"
+  context.stroke()
 
-  msg.selectAll("line")
-    .attr("x1", function(d) { return d.source.x })
-    .attr("y1", function(d) { return d.source.y })
-    .attr("x2", function(d) { return d.target.x })
-    .attr("y2", function(d) { return d.target.y })
-
+  context.beginPath()
+  nodes.forEach(drawNode)
+  context.globalAlpha = 1
+  context.fillStyle = d3.interpolateViridis(0.335)
+  context.fill()
 }
 
-const byIndex = r.propEq('index')
-// function update(){
-//   msg
-//     .transition()
-  
-// }
-
-// update()
-
-function dragstarted(d) {
-  if (!d3.event.active) simulation.alphaTarget(0.3).restart();
-  d.fx = d.x;
-  d.fy = d.y;
+function dragsubject() {
+  return simulation.find(d3.event.x, d3.event.y)
 }
 
-function dragged(d) {
-  d.fx = d3.event.x;
-  d.fy = d3.event.y;
+function dragstarted() {
+  if (!d3.event.active) simulation.alphaTarget(0.3).restart()
+  d3.event.subject.fx = d3.event.subject.x
+  d3.event.subject.fy = d3.event.subject.y
 }
 
-function dragended(d) {
-  if (!d3.event.active) simulation.alphaTarget(0);
-  d.fx = null;
-  d.fy = null;
+function dragged() {
+  d3.event.subject.fx = d3.event.x
+  d3.event.subject.fy = d3.event.y
+}
+
+function dragended() {
+  if (!d3.event.active) simulation.alphaTarget(0)
+  d3.event.subject.fx = null
+  d3.event.subject.fy = null
+}
+
+function drawLink(d) {
+  context.moveTo(d.source.x, d.source.y)
+  context.lineTo(d.target.x, d.target.y)
+}
+
+function drawNode(d) {
+  context.moveTo(d.x + 2.5, d.y)
+  context.arc(d.x, d.y, 2.5, 0, 2 * Math.PI)
 }
---
+++ svg.js
@@ -0,0 +1,208 @@
+const r = require("ramda")
+const d3 = require("d3")
+const rg = require('randomgraph')
+
+const document = require("global/document")
+const refreshButton = document.createElement('button')
+document.body.appendChild(refreshButton)
+const svg = d3.select("#canvas")
+svg.attr("preserveAspectRatio", "xMidYMid meet")
+
+const window = require("global/window")
+const width = +window.innerWidth
+svg.attr("width", width)
+const height =  +window.innerHeight
+svg.attr("height", height)
+
+// const G = {nodes:[{x:100}, {}, {}, {}]
+//   , links:[{source:0, target: 1, index:0}
+//           ,{source:1, target: 2, index:1}
+//           ,{source:2, target: 3, index:2}
+//           ]}
+//const G = rg.BalancedTree(3, 5)
+//const G = rg.WattsStrogatz.alpha(300, 4, 0.03)
+//const G = rg.ErdosRenyi.np(200, 0.)
+//const G = rg.BarabasiAlbert(300, 2, 1)
+const G = rg.BarabasiAlbert(60, 2, 2)
+const k = Math.sqrt(G.nodes.length / (width * height))
+const sideForce = 0.011
+function boundedByBox(x1, y1, x2, y2){
+  return function(){
+    return 0.2
+  }
+}
+const simulation = d3.forceSimulation()
+      .force("link", d3.forceLink().strength(boundedByBox(0, 0, width, height)))
+      .force("charge", d3.forceManyBody().strength(-0.5 / k))
+      .force("center", d3.forceCenter(width / 2, height / 2))
+//      .stop()
+
+simulation.nodes(G.nodes).on("tick", ticked)
+simulation.force("link").links(G.edges)
+
+var link = svg.append("g")
+  .attr("class", "links")
+  .selectAll("line")
+  .data(G.edges)
+  .enter().append("line")
+  .attr("stroke-width", function(d) { return 1 })
+
+var color = d3.scaleOrdinal(d3.schemeCategory20)
+
+var node = svg.append("g")
+.attr("class", "nodes")
+.selectAll("circle")
+.data(G.nodes)
+.enter().append("circle")
+.attr("r", 5)
+.attr("fill", function(d) { return 1 })
+.call(d3.drag()
+       .on("start", dragstarted)
+       .on("drag", dragged)
+       .on("end", dragended))
+
+node.append("title")
+ .text(function(d, id) { return id })
+
+const rn = require('random-number')
+const randEdge = rn.generator({min: 0, max: G.edges.length-1, integer: true})
+const randNode = rn.generator({min: 0, max: G.nodes.length-1, integer: true})
+const initNode = randNode()
+const touches = r.curry((node, link) => {
+  const z = link.source.index == node || link.target.index == node
+  // console.log(node, link, z, link.source.index, link.target.index)
+  return z
+})
+
+const messages = [initNode]
+
+function addDirections(link){
+  const to = messages.indexOf(link.source.index) > -1 ? link.target.index : link.source.index
+  const from = messages.indexOf(link.source.index) > -1 ? link.source.index : link.target.index
+  return r.merge({to, from}, link)
+}
+
+const currentEdges = G.edges.filter(touches(initNode)).map(addDirections)
+const drawnEdges = []
+// console.log(currentEdges)
+
+const msg = svg.append("g").attr("class", "msg")
+
+function update(currentEdges){
+  const t = d3.transition().duration(500)
+  const t0 = d3.transition().duration(0)
+  const line = msg.selectAll("line").data(currentEdges)
+  
+  line.enter().append("line")
+    .style("stroke", function(d) { return color(d.group)})
+    .transition(t)
+    .ease(d3.easeLinear)
+    .attr("stroke-width", function(d){ return 3 })
+    .on("start", function repeat() {
+      const message = this
+      d3.active(message)
+        .attrTween("x1", function(d) {
+          return function(t){
+            const p = link.data().find(byIndex(d.index))
+            return p.source.index == d.from ? p.source.x : p.target.x
+          }
+        })
+        .attrTween("y1", function(d) {
+          return function(t){
+            const p = link.data().find(byIndex(d.index))
+            return p.source.index == d.from ? p.source.y : p.target.y
+          }
+        })
+        .attrTween("x2", function(d) {
+          return function(t){
+            const p = link.data().find(byIndex(d.index))
+            const source = p.source.index == d.from ? p.source.x : p.target.x
+            const target = p.source.index == d.from ? p.target.x : p.source.x
+            return d3.interpolate(source, target)(t)
+          }
+        })
+        .attrTween("y2", function(d) {
+          return function(t){
+            const p = link.data().find(byIndex(d.index))
+            const source = p.source.index == d.from ? p.source.y : p.target.y
+            const target = p.source.index == d.from ? p.target.y : p.source.y
+            return d3.interpolate(source, target)(t)
+          }
+        })
+    })
+    .transition(t0)
+    .on("end", function(d){
+      const line = this
+      currentEdges.forEach((ie) => drawnEdges.push(ie))
+      const lastTargets = currentEdges.map(r.prop('to'))
+      // add nodes to messages 
+      currentEdges.forEach((edge) => {
+        if ( messages.indexOf(edge.from) == -1 ) {
+          messages.push(edge.from)
+        }
+        if ( messages.indexOf(edge.to) == -1 ) {
+          messages.push(edge.to)
+        }
+      })
+      // select all edges from messages that lead to nodes that are not in messages
+      // console.log(d.from, d.to, d)
+      // console.log(currentEdges)
+      // console.log( messages)
+      const extendingEdges = G.edges.filter((link) => {
+        const x = (messages.indexOf(link.source.index) > -1 ) ^ (messages.indexOf(link.target.index) > -1 )
+        // console.log(messages, link.source.index, link.target.index, x)
+        return x
+      })
+      // console.log(extendingEdges)
+      const nextEdges = extendingEdges.map(addDirections)
+      // console.log(nextEdges)
+      update(currentEdges.concat(nextEdges))
+    })
+}
+
+update(currentEdges)
+
+function ticked() {
+  link
+    .attr("x1", function(d) { return d.source.x })
+    .attr("y1", function(d) { return d.source.y })
+    .attr("x2", function(d) { return d.target.x })
+    .attr("y2", function(d) { return d.target.y })
+
+  node
+    .attr("cx", function(d) { return d.x })
+    .attr("cy", function(d) { return d.y })
+
+  msg.selectAll("line")
+    .attr("x1", function(d) { return d.source.x })
+    .attr("y1", function(d) { return d.source.y })
+    .attr("x2", function(d) { return d.target.x })
+    .attr("y2", function(d) { return d.target.y })
+
+}
+
+const byIndex = r.propEq('index')
+// function update(){
+//   msg
+//     .transition()
+  
+// }
+
+// update()
+
+function dragstarted(d) {
+  if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+  d.fx = d.x;
+  d.fy = d.y;
+}
+
+function dragged(d) {
+  d.fx = d3.event.x;
+  d.fy = d3.event.y;
+}
+
+function dragended(d) {
+  if (!d3.event.active) simulation.alphaTarget(0);
+  d.fx = null;
+  d.fy = null;
+}
