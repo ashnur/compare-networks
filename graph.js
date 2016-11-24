e551613cf26ca3661a38c549a42c460f61b68206
ashnur <ashnur@gmail.com>
2016-11-25 00:48:41 +0100

    wip

--- graph.js
+++ graph.js
@@ -1,9 +1,57 @@
-const rg = require('randomgraph')
+const BalancedTree = null
+const WattsStrogatz = null
+const ErdosRenyi = null
+const BarabasiAlbert = null
 
+const state = { algorithm: "BalancedTree"	
+							,	method: ""
+							, h: 3
+							, r: 3
+							, n: 3
+							, K: 3
+							, x: 3
+							, m0: 3
+							, M: 3
+							}
+
+const gui = new dat.GUI()
+const algorithm = gui.add(state, "algorithm", ["BalancedTree", "WattsStrogatz", "ErdosRenyi", "BarabasiAlbert"])
+const method = gui.add(state, "method", ["alpha", "beta", "nm", "np"])
+const bt = gui.addFolder('BalancedTree')
+let actualAlgo = ""
+
+let genGraph = () => {return{nodes:[], edges:[]}}
+
+let algoArgs = []
+
+algorithm.onChange((algo, ...args) => {
+	actualAlgo = algo
+	switch(algo){
+		case "BalancedTree":
+			genGraph = () => rg.BalancedTree(state.h, state.r)
+			break
+		case "WattsStrogatz":
+			genGraph = () => rg.WattsStrogatz[state.method](state.n, state.K, state.x)		
+			break
+		case "ErdosRenyi":
+			genGraph = () => rg.ErdosRenyi[state.method](state.n, state.x)		
+			break
+		case "BarabasiAlbert":
+			genGraph = () => rg.BarabasiAlbert(state.n, state.m0, state.M)		
+			break
+	}	
+})
+
+
+    
+const rg = require("randomgraph")
 //const graph = rg.BalancedTree(3, 2)
 //const graph = rg.WattsStrogatz.alpha(300, 4, 0.03)
 //const graph = rg.ErdosRenyi.np(200, 0.01)
-const graph = rg.BarabasiAlbert(3000, 2, 2)
+const graph = rg.BarabasiAlbert(300, 2, 2)
 //const graph = rg.BarabasiAlbert(60, 2, 2)
 
-module.exports = graph
+module.exports = function(cb){
+	const graph = genGraph(5,2)
+	cb(graph)
+}
--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const r = require("ramda")
 const d3 = require("d3")
 const document = require("global/document")
@@ -24,150 +23,160 @@ const moveBack = (node, i) => {
     node.vy = - dy * 3
   }
 }
-const limitToScreen = () => nodes.forEach(moveBack)
 
-const addDirections = (link) => {
-  const to = messages.indexOf(link.source) > -1 ? link.target : link.source
-  const from = messages.indexOf(link.source) > -1 ? link.source : link.target
-  return r.merge({to, from, p: 0}, link)
-}
+const G = require('./graph.js')
+G(function(graph){
 
-const touches = r.curry((node, link) => link.source == node || link.target == node )
+	if ( graph.edges.length <= 0 || graph.nodes.length <= 0 ) return
+	const limitToScreen = () => nodes.forEach(moveBack)
+	const addDirections = (link) => {
+		const to = messages.indexOf(link.source) > -1 ? link.target : link.source
+		const from = messages.indexOf(link.source) > -1 ? link.source : link.target
+		return r.merge({to, from, p: 0}, link)
+	}
 
-const inTheDark = (edge) => (messages.indexOf(edge.source) > -1 ) ^ (messages.indexOf(edge.target) > -1 )
+	const touches = r.curry((node, link) => link.source == node || link.target == node )
 
-
-const graph = require('./graph.js')
-const randNode = rn.generator({min: 0, max: graph.nodes.length-1, integer: true})
-const initNode = randNode()
-const messages = [initNode]
-let forceEdges = JSON.parse(JSON.stringify(graph.edges))
-let currentEdges = graph.edges.filter(touches(initNode)).map(addDirections)
-const drawnEdges = []
-const nodes = graph.nodes.map((value, id)=>{return {id,value}})
-const travelTime = 600 
+	const inTheDark = (edge) => (messages.indexOf(edge.source) > -1 ) ^ (messages.indexOf(edge.target) > -1 )
 
 
-const simulation = d3.forceSimulation()
-                     .force("center", d3.forceCenter(width / 2, height / 2))
-                     .force("link", d3.forceLink().distance(50).strength(0.02))
-                     .force("collide", d3.forceCollide(5))
-                     .force("charge", d3.forceManyBody().distanceMin(20).strength(-1))
-                     .force("boxed", limitToScreen)
 
-simulation.nodes(nodes).on("tick", ticked)
-simulation.force("link").links(forceEdges)
-canvas.call(d3.drag()
-              .container(canvas.node())
-              .subject(dragsubject)
-              .on("start", dragstarted)
-              .on("drag", dragged)
-              .on("end", dragended))
+	const randNode = rn.generator({min: 0, max: graph.nodes.length - 1 , integer: true})
+	const initNode = randNode()
+	const messages = [initNode]
+	let forceEdges = JSON.parse(JSON.stringify(graph.edges))
+	let currentEdges = graph.edges.filter(touches(initNode)).map(addDirections)
+	const drawnEdges = []
+	const nodes = graph.nodes.map((value, id)=>{return {id,value}})
+	const travelTime = 600 
 
-update()
 
-const notCurrentEdge = (fe) => currentEdges.find((ce) => ce.index != fe.index)
-const addToDrawn = (edge) => {
-  drawnEdges.push(edge)
-  if ( messages.indexOf(edge.from) == -1 ) {
-    messages.push(edge.from)
-  }
-  if ( messages.indexOf(edge.to) == -1 ) {
-    messages.push(edge.to)
-  }
-}
-function update(){
-  const timer = d3.timer(function(elapsed){
-    const progress = elapsed/travelTime
-    const moveMessage = (ce) => ce.p = d3.easeCubic(progress)
+			
 
-    if ( elapsed < travelTime ) {
-      if ( simulation.alpha() < 0.1 ) simulation.restart()
-      currentEdges.forEach(moveMessage)
-    } else {
-      timer.stop()
-      forceEdges = forceEdges.filter(notCurrentEdge)
-      currentEdges.forEach(addToDrawn) 
-      // select all edges from messages that lead to nodes that are not in messages
-      const extendingEdges = graph.edges.filter(inTheDark)
-      const nextEdges = extendingEdges.map(addDirections)
-      currentEdges = nextEdges
-      if ( currentEdges.length > 0 ) update()
-    }
-  })
-}
 
-const messageAlpha = 0.22
-const messageStrokeStyle = "#123456"
+	const simulation = d3.forceSimulation()
+											.force("center", d3.forceCenter(width / 2, height / 2))
+											.force("link", d3.forceLink().distance(50).strength(0.02))
+											.force("collide", d3.forceCollide(5))
+											.force("charge", d3.forceManyBody().distanceMin(20).strength(-1))
+											.force("boxed", limitToScreen)
 
-function ticked() {
-  context.clearRect(0, 0, width, height)
+	simulation.nodes(nodes).on("tick", ticked)
+	simulation.force("link").links(forceEdges)
+	canvas.call(d3.drag()
+								.container(canvas.node())
+								.subject(dragsubject)
+								.on("start", dragstarted)
+								.on("drag", dragged)
+								.on("end", dragended))
 
-  // context.globalAlpha = 0.1
-  // context.beginPath()
-  // forceEdges.forEach(drawLink)
-  // context.strokeStyle = "#666"
-  // context.stroke()
+	update()
 
-  context.globalAlpha = messageAlpha
-  context.beginPath()
-  drawnEdges.forEach(drawOldMessage)
-  context.strokeStyle = messageStrokeStyle
-  context.stroke()
 
-  context.beginPath()
-  currentEdges.forEach(drawMessage)
-  context.stroke()
+	const notCurrentEdge = (fe) => currentEdges.find((ce) => ce.index != fe.index)
+	const addToDrawn = (edge) => {
+		drawnEdges.push(edge)
+		if ( messages.indexOf(edge.from) == -1 ) {
+			messages.push(edge.from)
+		}
+		if ( messages.indexOf(edge.to) == -1 ) {
+			messages.push(edge.to)
+		}
+	}
+	function update(){
+		const timer = d3.timer(function(elapsed){
+			const progress = elapsed/travelTime
+			const moveMessage = (ce) => ce.p = d3.easeCubic(progress)
 
-  context.beginPath()
-  nodes.forEach(drawNode)
-  context.globalAlpha = 0.3
-  context.fillStyle = d3.interpolateViridis(0.3)
-  context.fill()
-}
+			if ( elapsed < travelTime ) {
+				if ( simulation.alpha() < 0.1 ) simulation.restart()
+				currentEdges.forEach(moveMessage)
+			} else {
+				timer.stop()
+				forceEdges = forceEdges.filter(notCurrentEdge)
+				currentEdges.forEach(addToDrawn) 
+				// select all edges from messages that lead to nodes that are not in messages
+				const extendingEdges = graph.edges.filter(inTheDark)
+				const nextEdges = extendingEdges.map(addDirections)
+				currentEdges = nextEdges
+				if ( currentEdges.length > 0 ) update()
+			}
+		})
+	}
 
-function drawOldMessage(message) {
-  const source = nodes[message.source]
-  const target = nodes[message.target]
-  context.moveTo(source.x, source.y)
-  context.lineTo(target.x, target.y)
-}
+	const messageAlpha = 0.22
+	const messageStrokeStyle = "#123456"
 
-function drawMessage(message) {
-  const source = nodes[message.from]
-  const target = nodes[message.to]
-  const {x, y} = d3.interpolate(source, target)(message.p)
-  context.moveTo(source.x, source.y)
-  context.lineTo(x, y)
-}
+	function ticked() {
+		context.clearRect(0, 0, width, height)
 
-function drawLink(d) {
-  context.moveTo(d.source.x, d.source.y)
-  context.lineTo(d.target.x, d.target.y)
-}
+		// context.globalAlpha = 0.1
+		// context.beginPath()
+		// forceEdges.forEach(drawLink)
+		// context.strokeStyle = "#666"
+		// context.stroke()
 
-function drawNode(d) {
-  context.moveTo(d.x + 2.5, d.y)
-  context.arc(d.x, d.y, 2.5, 0, 2 * Math.PI)
-}
+		context.globalAlpha = messageAlpha
+		context.beginPath()
+		drawnEdges.forEach(drawOldMessage)
+		context.strokeStyle = messageStrokeStyle
+		context.stroke()
 
-function dragsubject() {
-  return simulation.find(d3.event.x, d3.event.y)
-}
+		context.beginPath()
+		currentEdges.forEach(drawMessage)
+		context.stroke()
 
-function dragstarted() {
-  if (!d3.event.active) simulation.alphaTarget(0.3).restart()
-  d3.event.subject.fx = d3.event.subject.x
-  d3.event.subject.fy = d3.event.subject.y
-}
+		context.beginPath()
+		nodes.forEach(drawNode)
+		context.globalAlpha = 0.3
+		context.fillStyle = d3.interpolateViridis(0.3)
+		context.fill()
+	}
 
-function dragged() {
-  d3.event.subject.fx = d3.event.x
-  d3.event.subject.fy = d3.event.y
-}
+	function drawOldMessage(message) {
+		const source = nodes[message.source]
+		const target = nodes[message.target]
+		context.moveTo(source.x, source.y)
+		context.lineTo(target.x, target.y)
+	}
 
-function dragended() {
-  if (!d3.event.active) simulation.alphaTarget(0)
-  d3.event.subject.fx = null
-  d3.event.subject.fy = null
-}
+	function drawMessage(message) {
+		const source = nodes[message.from]
+		const target = nodes[message.to]
+		const {x, y} = d3.interpolate(source, target)(message.p)
+		context.moveTo(source.x, source.y)
+		context.lineTo(x, y)
+	}
+
+	function drawLink(d) {
+		context.moveTo(d.source.x, d.source.y)
+		context.lineTo(d.target.x, d.target.y)
+	}
+
+	function drawNode(d) {
+		context.moveTo(d.x + 2.5, d.y)
+		context.arc(d.x, d.y, 2.5, 0, 2 * Math.PI)
+	}
+
+	function dragsubject() {
+		return simulation.find(d3.event.x, d3.event.y)
+	}
+
+	function dragstarted() {
+		if (!d3.event.active) simulation.alphaTarget(0.3).restart()
+		d3.event.subject.fx = d3.event.subject.x
+		d3.event.subject.fy = d3.event.subject.y
+	}
+
+	function dragged() {
+		d3.event.subject.fx = d3.event.x
+		d3.event.subject.fy = d3.event.y
+	}
+
+	function dragended() {
+		if (!d3.event.active) simulation.alphaTarget(0)
+		d3.event.subject.fx = null
+		d3.event.subject.fy = null
+	}
+
+})
